007b65007899483dee673cddb4eac3a8a65a3dc6
Askerko Alexandr <askerko50@gmail.com>
2022-11-21 10:05:54 +0300

    fix(change BrowserRouter to HashRouter)

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-import {BrowserRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import {store} from "./Redux/redux-store";
 import App from "./App";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
     ,
     document.getElementById('root')
 );
