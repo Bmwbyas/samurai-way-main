3df3d6d30c0ef5a758987bb20a39a014bb452e77
Askerko Alexandr <askerko50@gmail.com>
2022-12-06 15:22:50 +0300

    fix (users search, view user,)

--- src/components/Profile/MyPosts/CommentForm/CommentForm.tsx
+++ src/components/Profile/MyPosts/CommentForm/CommentForm.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import s from "./CommentForm.module.css";
-import {Button, Col, Divider,  Row} from "antd";
-import {useForm, Controller, SubmitHandler} from "react-hook-form";
-import defaultAvatar from '../../../../assets/defaultAvatarUser.png'
+import {Button, Col, Divider, Row} from "antd";
+import {Controller, SubmitHandler, useForm} from "react-hook-form";
 import TextArea from "antd/es/input/TextArea";
 import {SendOutlined} from '@ant-design/icons';
+import {viewAvatar} from "../../../../utils/ViewAvatar/viewAvatar";
 
 interface IFormInput {
     text: string;
@@ -18,13 +18,13 @@ type AddpostFormType = {
     photo: string | null | undefined
 }
 
-export const CommentForm: React.FC<AddpostFormType> = ({submitForm, name, photo}) => {
+export const CommentForm: React.FC<AddpostFormType> = ({submitForm,  photo}) => {
     const {control, reset, formState: { isValid}, handleSubmit} = useForm<IFormInput>({mode: "onBlur"});
     const onSubmit: SubmitHandler<IFormInput> = data => {
         submitForm(data.text)
         reset()
     };
-    const avatar = photo ?? defaultAvatar
+    const avatar = viewAvatar(photo)
     const disableSendMessage=!isValid?s.sendButton:''
 
     return (
